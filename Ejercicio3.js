169cf5431a56a4cc3a8e23bf74d0cb784176342d
NicolasMuskusTarazona <nicolasmuskus1@gmail.com>
2025-04-07 08:59:08 -0500

    feat: :construction: `Ejercicio 3.`

---
+++ Ejercicio3.js
@@ -0,0 +1,5 @@
+// ### **3️⃣ Iteración y comparación de objetos**  
+// - [ ] **Iterar las propiedades de un objeto** y mostrarlas en consola.  
+// - [ ] **Comparar dos objetos** para verificar si son iguales.  
+// - [ ] **Comparar dos propiedades de objetos** para ver si son iguales.  
+
